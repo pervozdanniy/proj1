331c14eef38ddff1763f6dd88dffd29983d92b85
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2022-12-06 17:39:21 +0400

    SKOPA-122: change core main path

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -4,24 +4,21 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CoreModule } from './core.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    CoreModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        url: '0.0.0.0:50000',
-        package: 'core_service',
-        loader: {
-          keepCase: true,
-          longs: String,
-          enums: String,
-          defaults: true,
-          oneofs: true,
-        },
-        protoPath: join(__dirname, '../../../commands/_proto/core.proto'),
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(CoreModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50000',
+      package: 'core_service',
+      loader: {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
       },
+      protoPath: join(process.env.BASE_PATH, 'common/_proto', 'core.proto'),
     },
-  );
+  });
 
   return app.listen();
 }
