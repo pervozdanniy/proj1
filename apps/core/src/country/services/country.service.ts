eb2ef48dad096fc22cc7e0735a56c087e444755e
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-01-10 18:00:35 +0400

    SKOPA-87: eslint fix

--- apps/core/src/country/services/country.service.ts
+++ apps/core/src/country/services/country.service.ts
@@ -1,11 +1,8 @@
-import { Status } from '@grpc/grpc-js/build/src/constants';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CountryListResponse } from '~common/grpc/interfaces/country';
-import { GrpcException } from '~common/utils/exceptions/grpc.exception';
 import { CountryEntity } from '~svc/core/src/country/entities/country.entity';
-import { states } from '~svc/core/src/user/constants/states';
 
 @Injectable()
 export class CountryService {
