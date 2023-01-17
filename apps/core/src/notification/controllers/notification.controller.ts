5acfa9f364d54bd9e5bbc92376a7a0e234ac38a1
Danylo <bohdan-danylo.hushchyn@applicature.com>
2023-01-17 21:39:02 +0200

    SKOPA-258: removed useless variety of exception filters, used framework-based filters classes, removed unused arguments, send server metadata back if present

--- apps/core/src/notification/controllers/notification.controller.ts
+++ apps/core/src/notification/controllers/notification.controller.ts
@@ -1,4 +1,3 @@
-import { UseFilters } from '@nestjs/common';
 import {
   Notification,
   NotificationListResponse,
@@ -8,11 +7,9 @@ import {
   UpdateNotificationRequest,
 } from '~common/grpc/interfaces/notification';
 import { RpcController } from '~common/utils/decorators/rpc-controller.decorator';
-import { TypeOrmExceptionFilter } from '~common/utils/filters/type-orm-exception.filter';
 import { NotificationService } from '~svc/core/src/notification/services/notification.service';
 
 @RpcController()
-@UseFilters(TypeOrmExceptionFilter)
 @NotificationServiceControllerMethods()
 export class NotificationController implements NotificationServiceController {
   constructor(private notificationService: NotificationService) {}
--- common/utils/exceptions/grpc.exception.ts
+++ common/utils/exceptions/grpc.exception.ts
@@ -1,4 +1,4 @@
-import { status } from '@grpc/grpc-js';
+import { Metadata, status } from '@grpc/grpc-js';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 
@@ -12,18 +12,43 @@ const map: Partial<Record<HttpStatus, status>> = {
   [HttpStatus.INTERNAL_SERVER_ERROR]: status.INTERNAL,
 };
 
+const reverseMap: Partial<Record<status, HttpStatus>> = {
+  [status.OK]: HttpStatus.OK,
+  [status.CANCELLED]: HttpStatus.SERVICE_UNAVAILABLE,
+  [status.UNKNOWN]: HttpStatus.INTERNAL_SERVER_ERROR,
+  [status.INVALID_ARGUMENT]: HttpStatus.BAD_REQUEST,
+  [status.PERMISSION_DENIED]: HttpStatus.FORBIDDEN,
+  [status.NOT_FOUND]: HttpStatus.NOT_FOUND,
+  [status.ALREADY_EXISTS]: HttpStatus.CONFLICT,
+  [status.UNIMPLEMENTED]: HttpStatus.NOT_IMPLEMENTED,
+  [status.INTERNAL]: HttpStatus.INTERNAL_SERVER_ERROR,
+  [status.UNAVAILABLE]: HttpStatus.SERVICE_UNAVAILABLE,
+  [status.UNAUTHENTICATED]: HttpStatus.UNAUTHORIZED,
+};
+
+export const isGrpcException = (exception: Error): exception is GrpcException => {
+  return (exception as GrpcException)?.metadata instanceof Metadata;
+};
+
 export class GrpcException extends RpcException {
   static fromHttp(exception: HttpException) {
-    const resp = exception.getResponse();
-
-    return new GrpcException(
-      map[exception.getStatus()],
-      exception.message,
-      exception.getStatus(),
-      typeof resp === 'object' ? resp : undefined,
-    );
+    return new GrpcException(map[exception.getStatus()], exception.message);
   }
-  constructor(code: status, message: string, error_code?: number, additional_data?: any) {
-    super({ message, code, error_code, additional_data });
+
+  static toHttp(exception: GrpcException): HttpException {
+    return new HttpException(exception.details, reverseMap[exception.code], { cause: exception });
+  }
+
+  readonly code: status;
+
+  readonly details: string;
+
+  readonly metadata?: Metadata;
+
+  constructor(code: status, message: string, error_code?: number, metadata?: Metadata) {
+    super({ message, code, error_code });
+    this.code = code;
+    this.details = message;
+    this.metadata = metadata;
   }
 }
