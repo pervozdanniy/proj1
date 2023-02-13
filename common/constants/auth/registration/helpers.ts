e5a69ade71014240b3b5c8af0f68840ef95d5b92
Danylo <bohdan-danylo.hushchyn@applicature.com>
2023-02-13 12:47:11 +0200

    SKOPA-315: keep user session instead of loggin him out on successful registration

--- common/constants/auth/registration/helpers.ts
+++ common/constants/auth/registration/helpers.ts
@@ -3,3 +3,9 @@ import { PreRegisteredSessionInterface } from './interfaces';
 
 export const isPreRegistered = (session: SessionInterface): session is PreRegisteredSessionInterface =>
   !!(session as PreRegisteredSessionInterface)?.register;
+
+export const register = <User = any>(session: PreRegisteredSessionInterface, user: User): SessionInterface<User> => {
+  delete session.register;
+
+  return Object.assign(session, { user });
+};
