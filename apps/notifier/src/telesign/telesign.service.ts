7ffa7a82d70f195dfa5d5ee063373359b9a7ab73
Danylo <bohdan-danylo.hushchyn@applicature.com>
2023-02-16 13:12:08 +0200

    added missiong envs to .env.sample

--- apps/notifier/src/telesign/telesign.service.ts
+++ apps/notifier/src/telesign/telesign.service.ts
@@ -1,24 +1,19 @@
-import { Status } from '@grpc/grpc-js/build/src/constants';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { stringify } from 'qs';
 import { lastValueFrom } from 'rxjs';
-import { GrpcException } from '~common/utils/exceptions/grpc.exception';
 
 @Injectable()
 export class TelesignService {
   constructor(private readonly http: HttpService) {}
-  async send(phone, body) {
+  async send(phone: string, body: string) {
     const number = phone.replace('+', '');
     const data = stringify({
       phone_number: number,
       message: body,
       message_type: 'ARN',
     });
-    try {
-      await lastValueFrom(this.http.post('https://rest-api.telesign.com/v1/messaging', data));
-    } catch (e) {
-      throw new GrpcException(Status.ABORTED, 'Message provider error', 500);
-    }
+
+    return lastValueFrom(this.http.post('https://rest-api.telesign.com/v1/messaging', data));
   }
 }
