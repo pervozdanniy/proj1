d56b52381791db1494e6b9cc5562b64db8f42c5a
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-02-27 16:30:44 +0400

    SKOPA-348: return avatar

--- apps/core/src/db/migrations/1677138110571-AlterBalanceTable.ts
+++ apps/core/src/db/migrations/1677138110571-AlterBalanceTable.ts
@@ -4,7 +4,6 @@ export class AlterBalanceTable1677138110571 implements MigrationInterface {
   name = 'AlterBalanceTable1677138110571';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user_details" DROP COLUMN "avatar"`);
     await queryRunner.query(`ALTER TABLE "prime_trust_balance" DROP COLUMN "disbursable"`);
     await queryRunner.query(`ALTER TABLE "prime_trust_balance" DROP COLUMN "pending_transfer"`);
     await queryRunner.query(`ALTER TABLE "prime_trust_balance" DROP COLUMN "contingent_hold"`);
@@ -24,6 +23,5 @@ export class AlterBalanceTable1677138110571 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "prime_trust_balance" ADD "disbursable" double precision NOT NULL DEFAULT '0'`,
     );
-    await queryRunner.query(`ALTER TABLE "user_details" ADD "avatar" character varying`);
   }
 }
