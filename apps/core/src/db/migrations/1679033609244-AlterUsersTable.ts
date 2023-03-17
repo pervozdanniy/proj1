12df880b31bce41c7e4db7ee2a32a2ec8998f24b
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-03-17 11:37:02 +0400

    SKOPA-413: add alter users table

--- apps/core/src/db/migrations/1679033609244-AlterUsersTable.ts
+++ apps/core/src/db/migrations/1679033609244-AlterUsersTable.ts
@@ -1,18 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AlterUsersTable1679033609244 implements MigrationInterface {
-    name = 'AlterUsersTable1679033609244'
+  name = 'AlterUsersTable1679033609244';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_details" ADD "socure_verify" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "user_details" ADD "document_uuid" character varying(50)`);
-        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "send_type" SET DEFAULT '-1'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_details" ADD "socure_verify" boolean NOT NULL DEFAULT false`);
+    await queryRunner.query(`ALTER TABLE "user_details" ADD "document_uuid" character varying(50)`);
+    await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "send_type" SET DEFAULT '-1'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "send_type" SET DEFAULT '1'`);
-        await queryRunner.query(`ALTER TABLE "user_details" DROP COLUMN "document_uuid"`);
-        await queryRunner.query(`ALTER TABLE "user_details" DROP COLUMN "socure_verify"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "send_type" SET DEFAULT '1'`);
+    await queryRunner.query(`ALTER TABLE "user_details" DROP COLUMN "document_uuid"`);
+    await queryRunner.query(`ALTER TABLE "user_details" DROP COLUMN "socure_verify"`);
+  }
 }
