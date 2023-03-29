85c2600bd1558b50d0ea696bbe5330433c151ed0
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-03-29 11:51:09 +0400

    SKOPA-444: add country phone codes

--- apps/core/src/payment-gateway/country/data.ts
+++ apps/core/src/payment-gateway/country/data.ts
@@ -1,14 +1,15 @@
 export const countriesData: CountryData = {
-  US: { code: 'USA', currency_type: 'USD' },
-  CL: { code: 'CHL', currency_type: 'CLP' },
-  MX: { code: 'MEX', currency_type: 'MXN' },
-  CO: { code: 'COL', currency_type: 'COP' },
-  PE: { code: 'PER', currency_type: 'PEN' },
+  US: { code: 'USA', currency_type: 'USD', dial_code: '+1' },
+  CL: { code: 'CHL', currency_type: 'CLP', dial_code: '+56' },
+  MX: { code: 'MEX', currency_type: 'MXN', dial_code: '+52' },
+  CO: { code: 'COL', currency_type: 'COP', dial_code: '+57' },
+  PE: { code: 'PER', currency_type: 'PEN', dial_code: '+51' },
 };
 
 export type CountryData = {
   [code: string]: {
     code: string;
     currency_type: string;
+    dial_code: string;
   };
 };
