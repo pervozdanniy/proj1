5227c14c54e9f756e6daf94c39450973cfb57b0e
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-04-28 20:10:56 +0400

    SKOPA-15: add auth alter migration,delete destination

--- apps/auth/src/db/migrations-list.ts
+++ apps/auth/src/db/migrations-list.ts
@@ -1,9 +1,11 @@
 import { CreateAuthClient1671735244980 } from './migrations/1671735244980-CreateAuthClient';
 import { Create2FASettingsTable1674149141499 } from './migrations/1674149141499-Create2FASettingsTable';
 import { Alter2FARequireDestination1678273366673 } from './migrations/1678273366673-Alter2FARequireDestination';
+import { AlterAuthTable1681216028527 } from './migrations/1681216028527-AlterAuthTable';
 
 export default [
   CreateAuthClient1671735244980,
   Create2FASettingsTable1674149141499,
   Alter2FARequireDestination1678273366673,
+  AlterAuthTable1681216028527,
 ];
