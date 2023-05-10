ac21fbbc5e13f614dafddcda44942d0a07c57ea8
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-05-10 14:46:30 +0400

    SKOPA-22: eslint fix

--- apps/core/src/payment-gateway/services/koywe/managers/koywe-withdrawal.manager.ts
+++ apps/core/src/payment-gateway/services/koywe/managers/koywe-withdrawal.manager.ts
@@ -145,8 +145,6 @@ export class KoyweWithdrawalManager {
 
       return result.data;
     } catch (e) {
-      console.log(e);
-
       throw new GrpcException(Status.ABORTED, e.response.data.message, 400);
     }
   }
