5e740f39f04408adf28f2650c7879a32fbfad16b
Gevorg Poghosyan <gevorg.poghosyan@applicature.com>
2023-05-25 16:00:48 +0400

    SKOPA-29: add fees

--- apps/core/src/payment-gateway/services/liquido/managers/liquido-deposit.manager.ts
+++ apps/core/src/payment-gateway/services/liquido/managers/liquido-deposit.manager.ts
@@ -13,7 +13,7 @@ import { ConfigInterface } from '~common/config/configuration';
 import { Providers } from '~common/enum/providers';
 import { DepositRedirectData } from '~common/grpc/interfaces/payment-gateway';
 import { GrpcException } from '~common/utils/exceptions/grpc.exception';
-import {countriesData, liquidoFees} from '../../../country/data';
+import { countriesData, liquidoFees } from '../../../country/data';
 import { CreateReferenceRequest } from '../../../interfaces/payment-gateway.interface';
 import { CurrencyService } from '../../currency.service';
 import { KoyweMainManager } from '../../koywe/managers/koywe-main.manager';
