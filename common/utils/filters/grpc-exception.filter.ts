374af620fef01629ed485e35ff411ebbf8cafad4
Danylo <bohdan-danylo.hushchyn@applicature.com>
2023-05-31 16:53:43 +0300

    FIX: fixed logs

--- common/utils/filters/grpc-exception.filter.ts
+++ common/utils/filters/grpc-exception.filter.ts
@@ -3,7 +3,6 @@ import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
 import { isObject } from '@nestjs/common/utils/shared.utils';
 import { BaseRpcExceptionFilter } from '@nestjs/microservices';
 import * as Sentry from '@sentry/node';
-import axios from 'axios';
 import { Observable, throwError } from 'rxjs';
 import { EntityNotFoundError, QueryFailedError } from 'typeorm';
 import { GrpcException } from '../exceptions/grpc.exception';
@@ -36,11 +35,7 @@ export class BaseGrpcExceptionFilter extends BaseRpcExceptionFilter {
 export class AllExceptionFilter extends BaseGrpcExceptionFilter {
   catch(exception: any, host: ArgumentsHost): Observable<any> {
     if (process.env.NODE_ENV === 'dev') {
-      if (axios.isAxiosError(exception)) {
-        console.log('HTTP EX', exception.response.data);
-      } else {
-        console.log('EX', exception);
-      }
+      console.log('EX:', exception.response?.data ?? exception);
     }
     let finalException = exception;
 
